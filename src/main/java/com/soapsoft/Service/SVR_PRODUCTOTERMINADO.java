0f6a6205c17f5b084a584d853f60f6a8e3c4a0d9
JuanCRT92 <JuanCRT92>
2017-11-24 12:44:21 -0500

    Añado el dao de Producto Terminado con el servicio SRV_PRODUCTOTERMINADO

---
+++ src/main/java/com/soapsoft/Service/SVR_PRODUCTOTERMINADO.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.soapsoft.Service;
+
+import com.soapsoft.Dao.TbProductoTerminadoDaoImpl;
+import com.soapsoft.Model.TbProductoTerminado;
+import com.soapsoft.Model.TbUbicacion;
+import java.util.Date;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author PC
+ */
+@WebService(serviceName = "SVR_PRODUCTOTERMINADO")
+public class SVR_PRODUCTOTERMINADO {
+
+    
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+    
+     /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "fn_insertar_productoterminado")
+    public String fn_insertar_productoterminado(@WebParam(name = "referencia") String referencia, 
+                                                @WebParam(name = "descripcion") String descripcion,
+                                                @WebParam(name = "um") String um,
+                                                @WebParam(name = "stock") int stock,
+                                                @WebParam(name = "id_ubicacion") int id_ubicacion,
+                                                @WebParam(name = "creadorPor") String creadorPor) {
+        //TODO write your implementation code here:
+        
+        TbUbicacion obj=new TbUbicacion();
+        obj.setId(id_ubicacion);
+        
+        TbProductoTerminado obj2=new TbProductoTerminado(obj,referencia,descripcion, um, stock, creadorPor, new Date());
+        
+        try
+        {
+        TbProductoTerminadoDaoImpl dao=new TbProductoTerminadoDaoImpl();
+        dao.create(obj2);
+        return "Se inserto con exito el producto terminado";
+        }catch(Exception e)
+        {
+            System.out.println(e);
+        }
+       
+        return null;
+    }
+    
+   
+    
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "fn_eliminar_productoterminado")
+    public String fn_eliminar_productoterminado(@WebParam(name = "ID") int ID) {
+        //TODO write your implementation code here:
+      try
+      {
+        TbProductoTerminadoDaoImpl dao=new TbProductoTerminadoDaoImpl();
+               
+        TbProductoTerminado obj=dao.findById(ID);
+       
+        if(obj !=null)
+        {
+            dao.delete(obj);
+            return "Se elimino el registro";
+        }else
+        {
+            return "El registro no existe";
+        }
+          
+        }catch(Exception e)
+        {
+           throw e;
+        }
+
+    }  
+  
+    
+     /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "fn_modificar_productoterminado")
+    public String fn_modificar_productoterminado(@WebParam(name = "ID") int ID,
+                                                @WebParam(name = "referencia") String referencia, 
+                                                @WebParam(name = "descripcion") String descripcion,
+                                                @WebParam(name = "um") String um,
+                                                @WebParam(name = "stock") int stock,
+                                                @WebParam(name = "modificadoPor") String modificadoPor) {
+        //TODO write your implementation code here:
+      try
+      {
+        TbProductoTerminadoDaoImpl dao=new TbProductoTerminadoDaoImpl();  
+        TbProductoTerminado obj=dao.findById(ID);
+       
+        if(obj !=null)
+        {
+            obj.setReferencia(referencia);
+            obj.setDescripcion(descripcion);
+            obj.setUm(um);
+            obj.setStock(stock);
+            obj.setModificadoPor(modificadoPor);
+            obj.setModificadoEn(new Date());
+            
+            return "Se Actualizo Correctamente";
+        }else
+        {
+            return "El registro no exite";
+        }
+          
+        }catch(Exception e)
+        {
+           throw e;
+        }
+
+    } 
+    
+    
+    
+    
+    
+    
+}

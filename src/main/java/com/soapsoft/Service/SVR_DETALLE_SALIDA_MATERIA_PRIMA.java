5e78d06db5a93686036d9484b13e99c0a90afbc4
PC <PC@PC-PC>
2017-11-26 14:44:09 -0500

    Realizacion de SVR_detalle_salida_materia_prima

--- src/main/java/com/soapsoft/Service/SVR_DETALLE_SALIDA_MATERIA_PRIMA.java
+++ src/main/java/com/soapsoft/Service/SVR_DETALLE_SALIDA_MATERIA_PRIMA.java
@@ -47,5 +47,76 @@ public class SVR_DETALLE_SALIDA_MATERIA_PRIMA {
 
         
     }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "fn_delete_dtlle_salida_MP")
+    public String fn_delete_dtlle_salida_MP(@WebParam(name = "ID") int ID) {
+        //TODO write your implementation code here:
+       try
+      {
+        
+        TbDetalleSalidaMateriaPrimaDaoImpl dao=new TbDetalleSalidaMateriaPrimaDaoImpl();
+        
+        TbDetalleSalidaMateriaPrima obj=dao.findById(ID);
+       
+        if(obj !=null)
+        {
+            dao.delete(obj);
+            return "Se elimino la linea de detalle";
+        }else
+        {
+            return "La linea de detalle no existe";
+        }
+          
+        }catch(Exception e)
+        {
+           throw e;
+        }
+     
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "fn_update_dtlle_salida_materia_prima")
+    public String fn_update_dtlle_salida_materia_prima(@WebParam(name = "ID") int ID,@WebParam(name = "id_materia_prima") int id_materia_prima, @WebParam(name = "cantidad") int cantidad, @WebParam(name = "modificadoPor") String modificadoPor) {
+        
+        
+        
+        TbMateriaPrima objMateriaPrima= new TbMateriaPrima();
+        objMateriaPrima.setId(id_materia_prima);
+        
+          try {
+            TbDetalleSalidaMateriaPrimaDaoImpl dao=new TbDetalleSalidaMateriaPrimaDaoImpl();
+            TbDetalleSalidaMateriaPrima obj_modificar = dao.findById(ID);
+        
+        if(obj_modificar != null){
+                obj_modificar.setTbMateriaPrima(objMateriaPrima);
+                obj_modificar.setCantidad(cantidad);
+                obj_modificar.setModificadoPor(modificadoPor);
+                obj_modificar.setModificadoEn(new Date());            
+
+                dao.update(obj_modificar);
+                
+                return "Se Modifico con exito la linea de detalle";
+            }else{
+                return "Se actualizo la linea de detalle";
+            }
+            
+        
+        }catch(Exception e)
+        {
+           throw e;
+        }
+
+        
+        
+        
+        
+        
+        
+    }
     }
 
